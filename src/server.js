f414581a3ee2e4670e210bc880ab7195cceac511
Joaopedromata <joaopmata182@gmail.com>
2020-10-29 09:38:01 -0300

    init deploy

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => {
     console.log('The server is running on https://localhost:3333')
 })
